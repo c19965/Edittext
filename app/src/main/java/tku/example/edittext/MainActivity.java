0d3ba62724dbc1fb179e07b7d1293375e601def5
cindy <c85123072@gmail.com>
2019-04-11 13:03:40 +0800

    edit0411_1

--- app/src/main/java/tku/example/edittext/MainActivity.java
+++ app/src/main/java/tku/example/edittext/MainActivity.java
@@ -2,12 +2,39 @@ package tku.example.edittext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText ed1,ed2,ed3;
+    private TextView txv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    int size=15;
+    public void response(View v){
+        ed1 = findViewById(R.id.editText);
+        ed2 = findViewById(R.id.editText2);
+        ed3 = findViewById(R.id.editText3);
+        txv = findViewById(R.id.testView);
+
+        /*private float calculateBMI(float ht,float wt){
+            float BMI
+        }*/
+
+        float height = Float.parseFloat(ed2.getText().toString());
+        float weight = Float.parseFloat(ed3.getText().toString());
+        float BMI1 = calculateBMI(height,weight);
+
+
+
     }
 }
