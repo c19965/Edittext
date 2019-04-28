3de738f0836e6d71820a36c08ab707fa04e300a1
lihsin <c85123072@gmail.com>
2019-04-28 12:08:30 +0800

    update

--- app/src/main/java/tku/example/edittext/MainActivity.java
+++ app/src/main/java/tku/example/edittext/MainActivity.java
@@ -10,17 +10,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText ed1,ed2,ed3;
+    private EditText ed1, ed2, ed3;
     private TextView txv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        getWindow().setBackgroundDrawableResource(R.drawable.back);
     }
-    int size=15;
-    public void response(View v){
+
+    int size = 15;
+
+    public void response(View v) {
         ed1 = findViewById(R.id.editText);
         ed2 = findViewById(R.id.editText2);
         ed3 = findViewById(R.id.editText3);
@@ -34,7 +36,34 @@ public class MainActivity extends AppCompatActivity {
         float weight = Float.parseFloat(ed3.getText().toString());
         float BMI1 = calculateBMI(height,weight);
 
+        String BMI2 = String.valueOf(BMI1);
 
+        txv.setTextSize(size);
+        txv.setText(ed1.getText().toString() + ",Helllo\n" + "your BMI is: "+BMI2);
+        Toast.makeText(this,standardBMI(BMI1),Toast.LENGTH_LONG).show();
 
     }
+    private float calculateBMI(float height,float weight){
+        float BMI = 0;
+        BMI = weight / ((height/100)*(height/100));
+        return BMI;
+    }
+
+    private String standardBMI(double st){
+        String alert;
+
+        if(st>25){
+            alert = String.valueOf("Too Heavy!!!!");
+            getWindow().setBackgroundDrawableResource(R.drawable.heavy);
+        }else if(st<18.5){
+            alert = String.valueOf("Too Slim!!!!");
+            getWindow().setBackgroundDrawableResource(R.drawable.light);
+        }else{
+            alert = String.valueOf("Your body is good!");
+            getWindow().setBackgroundDrawableResource(R.drawable.normal);
+        }
+
+        return alert;
+    }
+
 }
